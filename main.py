82969711060f1aab6549f303fdc3818fd2d157e7
S.H.W <2536076378@qq.com>
2023-10-21 00:51:37 +0800

    exception handle

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # main.py
-import utils
+from utils import Colors, get_directory
 from excel_processor import ExcelProcessor
 from command_processor import CommandProcessor
 
 if __name__ == "__main__":
-    excel_processor = ExcelProcessor(utils.get_directory())
+    excel_processor = ExcelProcessor(get_directory())
     command_processor = CommandProcessor(excel_processor)
     command_processor.clean_console()
 
     while True:
-        command = input(utils.Colors.CYAN + "\n请输入命令（输入'help'查看可用指令）: " + utils.Colors.RESET).split()
-        if not command_processor.process_command(command):
-            break  # 如果process_command返回False，那么退出循环
+        try:
+            command = input(Colors.CYAN + "\n请输入命令（输入'help'查看可用指令）: " + Colors.RESET).split()
+            if not command_processor.process_command(command):
+                break  # 如果process_command返回False，那么退出循环
+        except Exception as e:
+            print(Colors.RED + f"程序执行时发生错误: {str(e)}" + Colors.RESET)
