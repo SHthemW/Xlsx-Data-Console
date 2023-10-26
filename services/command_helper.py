30e50ee5ae342eb67ac941875ed9a42aa21cf0d9
SHthemW <2536076378@qq.com>
2023-10-26 14:17:35 +0800

    opt: optimized the color of statement.

--- services/command_helper.py
+++ services/command_helper.py
@@ -4,59 +4,64 @@ from entities.color import Colors
 from entities.command import Command, Expression, Keyword
 
 
+class HelpType:
+    COMMAND = "command"
+    EXPRESSION = "expression"
+
+
 def print_help():
     print(f"\n{Colors.GREEN}目前可用的命令：{Colors.RESET}")
     print_help_message(
-        "Command", Command.FIND,
+        HelpType.COMMAND, Command.FIND,
         ['find command is used for finding value.',
          'you can also write <scope statement> as an option.'],
         ['find [value]', 'find [value] in [filename]']
     )
     print_help_message(
-        "Command", Command.OPEN,
+        HelpType.COMMAND, Command.OPEN,
         ['open command is used for open a file.',
          'the argument is a option to appoint a file name, no-args will automatic choose the latest file.'],
         ['open', 'open [filename]']
     )
     print_help_message(
-        "Command", Command.UPDATE,
+        HelpType.COMMAND, Command.UPDATE,
         ['update command is used for update some values or rows.',
          'use normal value to match values, and {} expr to match rows.',
          'Note: the <scope statement> expr is REQUIREMENT in update command.'],
         ['update [origValue] to [targetValue] in [filename]']
     )
     print_help_message(
-        "Command", Command.EXIT,
+        HelpType.COMMAND, Command.EXIT,
         ['exit the program immediately.'],
     )
     print_help_message(
-        "Command", Command.CLEAN,
+        HelpType.COMMAND, Command.CLEAN,
         ['clean the console texts that already showed.'],
     )
     print(f"\n{Colors.GREEN}表达式：{Colors.RESET}")
     print_help_message(
-        "Expression", Expression.FIELD,
+        HelpType.EXPRESSION, Expression.FIELD,
         ['field expr used for match a range of number.'],
         ['1001-1003 => [1001, 1002, 1003]']
     )
     print_help_message(
-        "Expression", " ".join(Expression.CHUNK),
+        HelpType.EXPRESSION, " ".join(Expression.CHUNK),
         ['field expr used for match a row with given condition.'],
         ['{Id=1001} => a row with a Id-1001 cell']
     )
 
 
 def print_help_message(cmd_type: str, cmd_name: str, message: List[str], example: List[str] = None):
-    print(f"\n{cmd_type} " + Colors.LIGHTMAGENTA_EX + f'{cmd_name}' + Colors.RESET + ":")
-    print("Describe:")
+    print(f"{Colors.LIGHTYELLOW_EX}\n{cmd_type} " + Colors.LIGHTMAGENTA_EX + f'{cmd_name}' + Colors.RESET + ":")
+    print(f"Describe:")
     for line in message:
         print(Colors.RESET + f'    {line}' + Colors.RESET)
     if not example:
         return
-    print("Examples:")
+    print(f"Examples:")
     for line in example:
         print("    " + " ".join(
-            [f'{Colors.LIGHTYELLOW_EX if (word == cmd_name or Keyword.is_keyword(word)) else Colors.RESET}'
+            [f'{Colors.LIGHTMAGENTA_EX if (word == cmd_name or Keyword.is_keyword(word)) else Colors.RESET}'
              f'{word}{Colors.RESET}'
              for word in line.split()]))
 
